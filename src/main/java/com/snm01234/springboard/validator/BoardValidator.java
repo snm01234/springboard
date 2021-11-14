16d121b35677ffcd87661f3f597604bbed22d54a
snm01234 <park96618@gmail.com>
2021-11-14 14:46:15 +0900

    form validator 작성, jpa로 db추가,수정

---
+++ src/main/java/com/snm01234/springboard/validator/BoardValidator.java
@@ -0,0 +1,24 @@
+package com.snm01234.springboard.validator;
+
+import com.snm01234.springboard.model.Board;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.thymeleaf.util.StringUtils;
+
+@Component
+public class BoardValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Board.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object obj, Errors errors) {
+        Board b = (Board) obj;
+        if(StringUtils.isEmpty(b.getContent())) {
+            errors.rejectValue("content", "key", "내용을 입력하세요");
+        }
+    }
+}
