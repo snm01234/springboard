a635d7e6e2947f630176d7d9f27102ad17e7d6e8
snm01234 <park96618@gmail.com>
2021-11-16 16:40:56 +0900

    Spring Security 이용한 로그인 처리

---
+++ src/main/java/com/snm01234/springboard/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.snm01234.springboard.repository;
+
+import com.snm01234.springboard.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
